b2fa24141f3a2c7f1f2169c3440a1eeefe4677be
shawn-phy <philipoyuga14@gmail.com>
2024-01-29 20:11:15 +0300

    setup React router and added more packages

---
+++ frontend/src/Home.js
@@ -0,0 +1,18 @@
+// home.js
+import React from "react";
+import './App.css'
+
+function Home(){
+
+    return (
+        <div className="home">
+        <header className="main-header">
+          <p>
+            Chat app 
+          </p>
+        </header>
+      </div>
+    );
+};
+
+export default Home; 
