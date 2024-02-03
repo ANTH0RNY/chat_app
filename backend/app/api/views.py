cdd1492ada825d6f07da8aac50a7fd8b34c6bf7b
ANTH0RNY <101851338+ANTH0RNY@users.noreply.github.com>
2024-02-03 18:23:45 +0300

    standardised error msg formatt added contact endpoint added a send message by username end point

--- backend/app/api/views.py
+++ backend/app/api/views.py
@@ -35,7 +35,7 @@ def login():
         return jsonify("Bad request"), 400
     user = User.query.filter_by(username=username).one_or_none()
     if not user or not user.verify_password(password):
-        return jsonify("Wrong username or password"), 401
+        return jsonify(msg="Wrong username or password"), 401
 
     access_token = create_access_token(identity=user)
     return jsonify(msg="login succeful", access_token=access_token)
@@ -64,14 +64,14 @@ def user():
     password = request.json.get("password", None)
 
     if username is None or password is None or password == "":
-        return jsonify("Bad request"), 400
+        return jsonify(msg="Bad request"), 400
     user = User.query.filter_by(username=username).one_or_none()
     if user is not None:
-        return jsonify("Wrong operation"), 405
+        return jsonify({"msg": "Wrong operation"}), 400
     new_user = User(username=username, password=password)
     db.session.add(new_user)
     db.session.commit()
-    return jsonify("User added successfully"), 201
+    return jsonify({"msg":"User added successfully"}), 201
 
 
 @api.route("/message/<int:id>", methods=["GET", "POST"])
@@ -82,7 +82,7 @@ def messages(id):
         sender_id= current_user.id
         recipient_id = id
         messages  = Message.query.filter(or_(and_(Message.sender == sender_id, Message.recipient == recipient_id), and_(Message.sender == recipient_id, Message.recipient == sender_id))).order_by(Message.date_created.desc()).all()
-        messages_object=[{'id': x.id, 'to': x.sender, 'from': x.recipient, 'body': x.body, 'date_created': x.date_created} for x in messages]
+        messages_object=[{'id': x.id, 'to': {"id": x.To.id, "username":x.To.username}, 'from': {"id":x.From.id, "username":x.From.username}, 'body': x.body, 'date_created': x.date_created} for x in messages]
         # print(len(messages_object))
         return jsonify(messages_object)
     if id == current_user.id:
@@ -96,6 +96,7 @@ def messages(id):
     try:
         db.session.commit()
     except IntegrityError:
+        db.session.roll_back()
         return jsonify({'msg': "something went wrong contact administrator"}), 500
 
     return jsonify({'msg': 'Message added successfully'}), 201
@@ -106,7 +107,30 @@ def messages(id):
 
 # User.query.join(Message, db.or_(Message.sender == User.id, Message.recipient == User.id))
 # .filter(db.or_(Message.sender == 2, Message.recipient==2)).filter(User.id != 2).order_by(Message.date_created).all()
+@api.route('/contacts')
+@jwt_required()
+def get_contacts():
+    users = db.session.query(User).join(Message, db.or_(Message.sender == User.id, Message.recipient == User.id))\
+            .filter(db.or_(Message.sender == current_user.id, Message.recipient== current_user.id)).filter(User.id != current_user.id).order_by(Message.date_created.desc()).all()
+    contacts = [{"id":x.id, "username": x.username} for x in users]
+    return jsonify(contacts)
 
+@api.route("/add_message/<string:username>", methods=["POST"])
+@jwt_required()
+def add_message_by_username(username):
+    user = User.query.filter_by(username = username).first()
+    body = request.json.get("body", None)
+    if user is None or body is None:
+        return jsonify({"msg": "bad request"}), 400
+    
+    message = Message(sender=current_user.id, recipient=user.id, body=body)
+    db.session.add(message)
+    try:
+        db.session.commit()
+    except IntegrityError:
+        db.session.roll_back()
+        return jsonify(msg="Something went wrong contact admininistrator"), 500
+    return jsonify({"msg": "Message added successfully"}), 201
 
 @api.route("/")
 def index():
