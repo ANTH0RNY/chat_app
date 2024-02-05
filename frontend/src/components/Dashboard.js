88556e93c9667161ecd27a9f97b3e7cf239ae595
shawn-phy <philipoyuga14@gmail.com>
2024-02-05 23:58:05 +0300

    f(frontend): added user parameter

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -2,7 +2,7 @@ import { useContext } from "react";
 import { AuthContext } from "../AuthContext";
 import { Navigate } from "react-router-dom"; 
 
-function Dashboard() {
+function Dashboard({user}) {
   const { token, loading } = useContext(AuthContext);
   if (loading) {
     return null;
@@ -12,7 +12,7 @@ function Dashboard() {
     return <Navigate to="/login" replace />;
   }
 
-  return <h1>Dashboard: welcome {} to your chat interface</h1>;
+  return <h1>Dashboard: welcome {user} to your chat interface</h1>;
 }
 
 export default Dashboard;
